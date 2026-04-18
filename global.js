b2a2f4232f237ebefcecb9d23653c24456a6608e
abbymcleod <a2mcleod@ucsd.edu>
2026-04-18 15:08:02 -0700

    new nav pages

--- global.js
+++ global.js
@@ -4,12 +4,40 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+let pages = [
+    { url: "", title: "Home" },
+    { url: "projects/", title: "Projects" },
+    { url: "contact/", title: "Contact" },
+    { url: "resume/", title: "Resume" },
+    { url: "https://github.com/abbymcleod", title: "GitHub" },
+  ];
 
-let currentLink = navLinks.find(
-    (a) => 
-        a.host === location.host && 
-        a.pathname === location.pathname
-);
+const BASE_PATH =
+   location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/portfolio/";
 
-currentLink?.classList.add('current');
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+    let a = document.createElement("a");
+    a.href = url;
+    a.textContent = p.title;
+
+    a.classList.toggle(
+        "current",
+        a.host === location.host &&
+          a.pathname.replace(/\/$/, "") === location.pathname.replace(/\/$/, "")
+      );
+
+      if (a.host !== location.host) {
+        a.target = "_blank";
+      }
+    
+      nav.append(a);
+}
