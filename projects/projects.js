c8e55e8678c942f492da0a872e9863c0c15f6c53
abbymcleod <a2mcleod@ucsd.edu>
2026-05-01 23:01:00 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,17 +1,20 @@
 import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+const projects = await fetchJSON('../lib/projects.json');
+
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let data = [
-    { value: 1, label: 'apples' },
-    { value: 2, label: 'oranges' },
-    { value: 3, label: 'mangos' },
-    { value: 4, label: 'pears' },
-    { value: 5, label: 'limes' },
-    { value: 5, label: 'cherries' },
-  ];
+let rolledData = d3.rollups(
+    projects,
+    (v) => v.length,
+    (d) => d.year,
+  );
   
-  let sliceGenerator = d3.pie().value((d) => d.value);
+let data = rolledData.map(([year, count]) => {
+    return { value: count, label: year };
+});
+
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
@@ -32,7 +35,6 @@ data.forEach((d, idx) => {
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
 
-const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
