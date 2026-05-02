d7d7479d7a67d2b64cfbc0991f610c693697e313
abbymcleod <a2mcleod@ucsd.edu>
2026-05-01 22:41:37 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -2,9 +2,18 @@ import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcs.forEach((arc, idx) => {
+  d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors[idx]);
 });
 
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
