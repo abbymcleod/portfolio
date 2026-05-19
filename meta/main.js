ac342dbabfab853339f63c8cf8e7814b58f01c56
abbymcleod <a2mcleod@ucsd.edu>
2026-05-19 16:15:03 -0700

    sort files

--- meta/main.js
+++ meta/main.js
@@ -307,7 +307,8 @@ function renderCommitInfo(data, commits) {
       .groups(lines, (d) => d.file)
       .map(([name, lines]) => {
         return { name, lines };
-      });
+      })
+      .sort((a,b) => b.lines.length - a.lines.length);
   
     let filesContainer = d3
       .select('#files')
