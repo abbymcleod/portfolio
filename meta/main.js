9020c5c5c0bb27a15ad36676901fb029b9d95364
abbymcleod <a2mcleod@ucsd.edu>
2026-05-22 17:21:48 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -158,7 +158,7 @@ function renderCommitInfo(data, commits) {
       .append('g')
       .attr('transform', `translate(0, ${usableArea.bottom})`)
       .attr('class', 'x-axis')
-      .call(d3.axisBottom(xScale).ticks(8))
+      .call(d3.axisBottom(xScale).ticks(d3.timeMonth.every(1)))
       .selectAll('text')
       .style('font-size', '16px')
 
